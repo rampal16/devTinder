bc7c234ef255c9c1bcaafc8441bde9d516f007fe
Ram Pal <ram.pal1308@gmail.com>
2024-11-05 18:40:41 +0530

    schema and models

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 const express = require("express");
 
+const connectDB = require("./config/database");
+const User = require("./models/user");
+
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello from server 1")
+
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: 'Ram',
+        lastName: 'Pal',
+        emailId: 'something@gmail.com',
+        password: 'hello@123'
+    })
+
+    try {
+        await user.save();
+        res.send("User added successfully")
+    } catch (error) {
+        res.status(400).send("Error saving the user", error.message)
+    }
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-});
+
+connectDB()
+    .then(() => {
+        console.log('Databse connection is successful !!');
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000")
+        });
+    }).catch(() => {
+        console.log('Failed to connect with database.')
+    })
+
+
