508bfca8c8764a079222a49ddae416c3d502da05
Ram Pal <ram.pal1308@gmail.com>
2024-11-22 14:56:22 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -5,23 +5,81 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: 'Ram',
-        lastName: 'Pal',
-        emailId: 'something@gmail.com',
-        password: 'hello@123'
-    })
+    const user = new User(req.body);
 
     try {
         await user.save();
         res.send("User added successfully")
     } catch (error) {
-        res.status(400).send("Error saving the user", error.message)
+        res.status(400).send(error.message)
     }
 })
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("User not found !!")
+        } else {
+            res.send(users);
+        }
+
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            res.status(404).send("User not found !!")
+        } else {
+            res.send(users);
+        }
+
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully.")
+
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const data = req.body;
+
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const updateAllowed = Object.keys(data).every(key => ALLOWED_UPDATES.includes(key));
+
+    try {
+        if (!updateAllowed) {
+            throw new Error("Update not allowed.")
+        }
+        await User.findByIdAndUpdate({ _id: userId }, data, {
+            runValidators: true
+        });
+        res.send("User updated successfully.")
+
+    } catch (error) {
+        res.status(400).send("Update failed: "+error.message)
+    }
+})
 
 connectDB()
     .then(() => {
@@ -32,5 +90,3 @@ connectDB()
     }).catch(() => {
         console.log('Failed to connect with database.')
     })
-
-
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,46 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
-    passoword: {
-        type: String
+    password: {
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!['male', 'female', 'others'].includes(value)) {
+                throw new Error("Invalid Gender detail.")
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/024/983/914/small/simple-user-default-icon-free-png.png"
+    },
+    about: {
+        type: String,
+        default: "This is about me..."
+    },
+    skills: {
+        type: [String]
     }
-})
+}, { timestamps: true})
 
 const User = mongoose.model("User", userSchema);
 
